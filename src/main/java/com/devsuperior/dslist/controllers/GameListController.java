96516b185b1c7e69cf7c0af89b382213a57a72b1
Guilherme Guimarães de Medeiros <gg.medeiros2011@gmail.com>
2023-07-12 20:47:01 -0300

    SQL, projection, get games by list

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameListDto;
+import com.devsuperior.dslist.dto.GameMinDto;
 import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -17,7 +20,8 @@ public class GameListController {
 	@Autowired
 	private GameListService gameListService;
 	
-	
+	@Autowired
+	private GameService gameService;
 	
 	@GetMapping
 	public List<GameListDto> findAll() {
@@ -25,5 +29,10 @@ public class GameListController {
 		return result;
 	}
 		
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDto> findByList(@PathVariable Long listId) {
+		List<GameMinDto> result = gameService.findByList(listId);
+		return result;
+	}
 
 }
